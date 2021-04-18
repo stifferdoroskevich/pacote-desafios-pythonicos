c428551e5e52942df67ab951b98156e29960916b
Stiffer Doroskevich <stiffer17@gmail.com>
2021-04-17 22:52:58 -0400

    Ex 4 and 5 OK

--- 05_verbing.py
+++ 05_verbing.py
@@ -10,9 +10,13 @@ Se o tamanho da string for menor que 3, não altere nada.
 Retorne o resultado da string.
 """
 
+
 def verbing(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) >= 3:
+        if s[-3:] == 'ing':
+            return s + 'ly'
+        return s + 'ing'
+    return s
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
