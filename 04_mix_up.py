bf23edae922d1e525b5601a7ab89db7b509a4833
Stiffer Doroskevich <stiffer17@gmail.com>
2021-04-17 23:07:35 -0400

    EX 04, improving performance

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -14,7 +14,7 @@ Assuma que a e b tem tamanho 2 ou maior.
 
 
 def mix_up(a, b):
-    return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
+    return ''.join([b[:2], a[2:], ' ', a[:2], b[2:]])
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
